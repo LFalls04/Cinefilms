47591dc19699e035a325037cf0c1e4cba7153224
Lathan Falls <lathanfalls04@gmail.com>
2025-01-29 02:23:00 -0600

    fixed api key issue

--- cinefilms-app/src/components/MovieCard.jsx
+++ cinefilms-app/src/components/MovieCard.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const MovieCard = ({ movie:
-  { title, vote_average, poster_path, release_date, original_language }
+const MovieCard = ({
+  movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
   return (
     <div className="movie-card">
       <img
-        src={poster_path ?
-          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "/no-movie.png"
+        }
         alt={title}
       />
 
@@ -17,7 +21,7 @@ const MovieCard = ({ movie:
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
 
           <span>•</span>
@@ -25,11 +29,22 @@ const MovieCard = ({ movie:
 
           <span>•</span>
           <p className="year">
-            {release_date ? release_date.split('-')[0] : 'N/A'}
+            {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
         </div>
       </div>
     </div>
-  )
-}
-export default MovieCard
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    vote_average: PropTypes.number,
+    poster_path: PropTypes.string,
+    release_date: PropTypes.string,
+    original_language: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
